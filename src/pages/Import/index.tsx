5c95d61d993511cec309414663947aedee2725b0
Rodrigo Quintam <rquintam@gmail.com>
2021-01-13 18:03:34 -0300

    Update Routes

---
+++ src/pages/Import/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Import: React.FC = () => {
+  return <h1>Import</h1>;
+};
+
+export default Import;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
+import Import from '../pages/Import';
 import Diploma from '../pages/Diploma';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    <Route path="/import" component={Import} />
     <Route path="/diploma" component={Diploma} />
   </Switch>
 );
